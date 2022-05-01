4b7a87dbc842a8bdab4fd33de53b37d947bc4da2
yogesh-2117 <yogeshburada13@gmail.com>
2022-05-01 15:51:31 +0530

    weather app added image

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const { response } = require("express");
+// const { response } = require("express");
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser")
 
 const app = express();
 
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
+
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html")
 })
@@ -24,16 +27,21 @@ app.post('/Weather.html',(req,res)=>{
             const Description = WeatherData.weather[0].description
             const temp = WeatherData.main.temp
             const icon = WeatherData.weather[0].icon
+            const feelsLike = WeatherData.main.feels_like
+            const humidity = WeatherData.main.humidity
+            
             const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
-            res.write(`<h1>${Description}</h1>
-                        <p>${temp}<p>`)
-            res.write("<img src = "+imageURL+">");
+            res.write(`<h1>Weather at ${query}:${Description}</h1>`)
+            res.write(`<img src = ${imageURL}>`)
+            res.write(`<h2>Temparature is ${temp} degrees celcius</h2>
+                        <p>Feels Like ${feelsLike} degrees celcius</p>
+                        <p>Humidity is around ${humidity}</p>`)
             res.send()
         })
     })
 })
      
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("server is running on port 3000.")
 })
